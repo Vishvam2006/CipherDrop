55e6151b2483c4873ae16c4f45a0f02bbd309be9
Vishvam2006 <vishvam.r.modi@gmail.com>
2026-03-26 22:55:21 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -2,41 +2,40 @@ import express from "express";
 import mongoose from "mongoose";
 import { ENV } from "./lib/env.js";
 import uploadRoute from "./routes/upload.js";
-import cors from "cors"
+import cors from "cors";
 import connectToDatabase from "./db/db.js";
-import authRoutes from "./routes/auth.js"
-import "./utils/cron.js"
-
-connectToDatabase();
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-
-
+import authRoutes from "./routes/auth.js";
+import "./utils/cron.js";
 import cors from "cors";
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://cipher-drop-file-share.vercel.app"
+  "https://cipher-drop-file-share.vercel.app",
 ];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like Postman)
-    if (!origin) return callback(null, true);
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
 
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed: " + origin));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed: " + origin));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectToDatabase();
 
 app.use("/api", uploadRoute);
 app.use("/api/auth", authRoutes);
